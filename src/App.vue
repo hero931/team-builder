ac99ea190cae3b63d987ae620ca5cea51a0d7216
Ilya Andreev <ilyaa1982@gmail.com>
2021-04-14 00:42:42 -0400

    app.vue have been updated

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,43 @@
 <template>
-    <router-view></router-view>
+    <div class="box">
+        <div class="nav"><the-header></the-header></div>
+        <router-view></router-view>
+    </div>
 </template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+export default {
+  components: { TheHeader },    
+}
+</script>
+
+<style>
+    * {
+        box-sizing: border-box;
+    }
+
+    html {
+        font-family: "Roboto", sans-serif;
+    }    
+
+    .box {
+        display: grid;
+        grid-template-columns: repeat(4, 25%);
+        grid-template-rows: 5rem 10rem 10rem;
+        grid-template-areas: 
+            "alef alef alef alef"
+            "bet bet bet gimel"
+            "dal dal dal dal";
+    }
+
+    .nav {        
+        border: 1px solid lightgray;
+        grid-area: alef;
+        border-radius: 8px;
+        margin: 0.3rem;
+        box-shadow: 5px 10px lightgray;
+    }
+
+    
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,58 @@
+<template>
+    <header>
+        <nav>
+            <h2 id="a1"><router-link to="/">Team builder</router-link></h2>        
+            <ul>
+                <li id="a2">
+                    <router-link to="/profs">Professionals</router-link>
+                </li>
+                <li id="a3">
+                    <router-link to="/requests">Requests</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+    
+    header {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 7fr 1.5fr;        
+    }
+
+    header h2 {
+        margin-left: 2rem;
+    }
+    
+
+    header a {
+        text-decoration: none;          
+    }
+
+    header nav {        
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    header ul {
+        list-style: none;
+        margin: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    li {
+        margin: 0 0.7rem;       
+    }
+
+    a:active,
+    a:hover,
+    a.router-link-active {
+        text-decoration: underline;
+        text-decoration-color: darkorange;        
+    }
+</style>
