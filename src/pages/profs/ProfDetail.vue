5d4439c831c1a51d79b00004ee913704ff6ab5e8
Ilya Andreev <ilyaa1982@gmail.com>
2021-04-14 10:27:38 -0400

    routes added

--- src/pages/profs/ProfDetail.vue
+++ src/pages/profs/ProfDetail.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
         Details about professional
+        <router-view></router-view>
+        <router-link to="/profs/1/contact">Contact</router-link>
     </div>
 </template>
--- src/pages/profs/ProfsList.vue
+++ src/pages/profs/ProfsList.vue
@@ -1,3 +1,12 @@
 <template>
-    <section>Filter</section>
+    <div>
+        <section>Filter</section>
+        <section>
+            <div class="controls">
+                <button>Refresh</button>
+                <router-link to="/register">Register a Pro</router-link>
+            </div>
+            <ul>List of Professionals</ul>
+        </section>
+    </div>
 </template>
