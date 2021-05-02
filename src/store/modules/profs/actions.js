837b406fd27d3e135b194b9b474ee676efd18453
Ilya Andreev <31460531+hero931@users.noreply.github.com>
2021-05-01 21:05:49 -0400

    put request added to send data to server

--- src/store/modules/profs/actions.js
+++ src/store/modules/profs/actions.js
@@ -1,13 +1,28 @@
 export default {
-    registerProf(context, data) {
-        const profData = {
-            id: context.rootGetters.userId,
+    async registerProf(context, data) {
+        const userId = context.rootGetters.userId;
+        const profData = {            
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
             hourlyRate: data.rate,
             areas: data.areas
         };
-        context.commit('registerProf', profData);
+
+        const response = await fetch(`https://professionals-builder-default-rtdb.firebaseio.com/profs/${userId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(profData)
+        });
+
+        //const responseData = await response.json();
+
+        if(!response.ok) {
+            //error...
+        }
+
+        context.commit('registerProf', {
+            ...profData,
+            id: userId
+        });
     }
 };
