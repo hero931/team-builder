b84adb3ae1f6e19d89f5bf5179e0fe01c175ccbe
Ilya Andreev <31460531+hero931@users.noreply.github.com>
2021-06-05 10:06:07 -0400

    store auth have been updated

--- src/store/index.js
+++ src/store/index.js
@@ -3,23 +3,15 @@ import { createStore } from 'vuex'
 import profsModule from './modules/profs/index.js'
 import reqModule from './modules/requests/index.js'
 import eventModule from './modules/events/index.js'
+import authModule from './modules/auth/index.js'
 
 const store = createStore({
     modules: {
         profs: profsModule,
         requests: reqModule,
-        events: eventModule
-    },
-    state() {
-        return {
-            userId: 'c1'
-        };
-    },
-    getters: {
-        userId(state) {
-            return state.userId;
-        }
-    }
+        events: eventModule,
+        auth: authModule
+    }       
 });
 
 export default store;
